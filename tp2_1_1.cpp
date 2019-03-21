a0653a36fe06293e6f49169d1d8c79af3fb3ceaf
Alejandro <ale_arc2015@hotmail.com>
2019-03-21 19:36:59 -0300

    Arreglado

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -7,14 +7,13 @@ int main(void)
 {
 	int f,c;
 	double mt[N][M];
-
-	for (f = 0; f < N; f++)
-	{
-		for (f = 0; f < N; f++)
-		{
-			cprintf("%lf   ", mt[f][c]);
-		}
-		cprintf("\n",);
+	printf("Carga y muestra la matriz\n");
+	for(f=0; f<N ; f++){
+		for (c = 0; c<M; c++){
+		mt[f][c]=rand();
+		printf("%lf   ", mt[f][c]);	
+			}
+			printf("\n");
 	}
 	return 0;
 }
